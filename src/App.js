1d6c49a455a4803a9075e8a38a780026e150f82a
Mike <slavik433@gmail.com>
2021-11-26 16:04:31 +0300

    lesson 11

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
-import {Tecnologies} from "./Tecnologies";
 import {Header} from "./Header";
+import {Main} from "./Tecnologies";
 
 
 const App = () => {
     return (
-        <div className="App">
-            <Header />
-            <Tecnologies />
+        <div className="app-wrapper">
+            <header className='header'>
+                Header
+                <img src="https://pngimg.com/uploads/tesla_logo/tesla_logo_PNG16.png"/>
+            </header>
+            <nav className='nav'>
+                <div><a href="#">Profile</a></div>
+                <div><a href="#">Messages</a></div>
+                <div><a href="#">News</a></div>
+                <div><a href="#">Settings</a></div>
+            </nav>
+            <div className='content'>
+                <img src="https://static3.depositphotos.com/1000454/256/i/600/depositphotos_2567474-stock-photo-wide-panorama-of-french-alps.jpg"/>
+                <div>Content</div>
+            </div>
         </div>
     )
 }
-
-
-
-
 
 export default App;
--- src/Tecnologies.js
+++ src/Tecnologies.js
@@ -1,4 +1,4 @@
-export const Tecnologies = () => {
+export const Main = () => {
     return(
         <div>
             KobraKPSS
