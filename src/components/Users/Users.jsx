fea7634c263c14e82a8e2c83678430301038a8c8
KobraMik <slavik433@gmail.com>
2022-03-13 21:04:26 +0300

    lesson 52: get data items in users button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,16 @@ import * as axios from 'axios';
 
 
 let Users = (props) => {
-    debugger
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items);
-            });
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
+        }
     }
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {props.users.map(m => <div key={m.id}>
             <span>
                 <div>
