a69041b8f3d39685ea6aec0d97cc58c2d2dce4c0
Mike <slavik433@gmail.com>
2022-06-21 14:59:10 +0300

    Updates

---
+++ precache-manifest.68d0289e6323cb59f008e01040ef59ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244242b533616e720744a793b37482ef",
+    "url": "/wayofthesamurai/index.html"
+  },
+  {
+    "revision": "ff99edea32684edb8690",
+    "url": "/wayofthesamurai/static/css/main.de1822ba.chunk.css"
+  },
+  {
+    "revision": "3c90726b40c0b9675e43",
+    "url": "/wayofthesamurai/static/js/2.b9532194.chunk.js"
+  },
+  {
+    "revision": "ff99edea32684edb8690",
+    "url": "/wayofthesamurai/static/js/main.78d9d7ff.chunk.js"
+  },
+  {
+    "revision": "9e99d626d552cda8eeca",
+    "url": "/wayofthesamurai/static/js/runtime~main.92d09e46.js"
+  },
+  {
+    "revision": "0b1a2a602715b40925a992ab873f7798",
+    "url": "/wayofthesamurai/static/media/Circle-Loading.0b1a2a60.svg"
+  }
+]);
